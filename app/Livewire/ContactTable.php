566972d67f99635b31f2aadb0d8c95c751b8f397
Kefka-dev <sith.patri@gmail.com>
2025-05-03 22:32:41 +0200

    feat: table with user contacts component made with powergrids

---
+++ app/Livewire/ContactTable.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Contact;
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Blade;
+use PowerComponents\LivewirePowerGrid\Button;
+use PowerComponents\LivewirePowerGrid\Column;
+use PowerComponents\LivewirePowerGrid\Facades\Filter;
+use PowerComponents\LivewirePowerGrid\Facades\PowerGrid;
+use PowerComponents\LivewirePowerGrid\PowerGridFields;
+use PowerComponents\LivewirePowerGrid\PowerGridComponent;
+
+final class ContactTable extends PowerGridComponent
+{
+    public string $tableName = 'contact-table-4gkobf-table';
+
+    protected $listeners = [
+        'contact-added' => '$refresh',
+    ];
+
+    public function setUp(): array
+    {
+        $this->showCheckBox();
+
+        return [
+            PowerGrid::header()
+                ->showSearchInput(),
+            PowerGrid::footer()
+                ->showPerPage()
+                ->showRecordCount(),
+        ];
+    }
+
+    public function datasource(): Builder
+    {
+        return Contact::query();
+    }
+
+    public function relationSearch(): array
+    {
+        return [];
+    }
+
+    public function fields(): PowerGridFields
+    {
+        return PowerGrid::fields()
+            ->add('id')
+            ->add('name')
+            ->add('email')
+            ->add('use_vykanie')
+            ->add('use_vykanie_formatted', function ($row) {
+                $blade = $row->use_vykanie
+                    ? '<flux:badge color="amber">Vy</flux:badge>'
+                    : '<flux:badge color="lime">Ty</flux:badge>';
+                return Blade::render($blade);
+            })
+            ->add('oslovenie')
+            ->add('user_id')
+            ->add('created_at');
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make('Id', 'id'),
+            Column::make('Name', 'name')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Email', 'email')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Formálnosť', 'use_vykanie_formatted', 'use_vykanie')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Oslovenie', 'oslovenie')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('User id', 'user_id'),
+
+            Column::make('Added', 'created_at')
+                ->sortable()
+                ->searchable(),
+
+            Column::action('Action')
+        ];
+    }
+
+    public function filters(): array
+    {
+        return [
+        ];
+    }
+
+    #[\Livewire\Attributes\On('edit')]
+    public function edit($rowId): void
+    {
+        $this->js('alert('.$rowId.')');
+    }
+
+    public function actions(Contact $row): array
+    {
+        return [
+//            Button::add('edit')
+//                ->slot('Edit: '.$row->id)
+//                ->id()
+//                ->class('pg-btn-white dark:ring-pg-primary-600 dark:border-pg-primary-600 dark:hover:bg-pg-primary-700 dark:ring-offset-pg-primary-800 dark:text-pg-primary-300 dark:bg-pg-primary-700')
+//                ->dispatch('edit', ['rowId' => $row->id]),
+            Button::add('delete')
+                ->slot('Delete')
+                ->id()
+                ->class('relative items-center font-medium justify-center gap-2 whitespace-nowrap disabled:opacity-75 dark:disabled:opacity-75 disabled:cursor-default disabled:pointer-events-none h-10 text-sm rounded-lg px-4 inline-flex  bg-red-500 hover:bg-red-600 dark:bg-red-600 dark:hover:bg-red-500 text-white  shadow-[inset_0px_1px_var(--color-red-500),inset_0px_2px_--theme(--color-white/.15)] dark:shadow-none [[data-flux-button-group]_&]:border-e [:is([data-flux-button-group]>&:last-child,_[data-flux-button-group]_:last-child>&)]:border-e-0 [[data-flux-button-group]_&]:border-red-600 dark:[[data-flux-button-group]_&]:border-red-900/25')
+                ->dispatch('open-delete-modal', ['contactId' => $row->id]),
+        ];
+    }
+
+    /*
+    public function actionRules($row): array
+    {
+       return [
+            // Hide button edit for ID 1
+            Rule::button('edit')
+                ->when(fn($row) => $row->id === 1)
+                ->hide(),
+        ];
+    }
+    */
+}
