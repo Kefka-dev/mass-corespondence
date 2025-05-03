27e4368d4db0038ae4a330fbfe68c5dd07f02873
Kefka-dev <sith.patri@gmail.com>
2025-05-03 22:33:57 +0200

    feat: cantacts view

---
+++ resources/views/contactss.blade.php
@@ -0,0 +1,4 @@
+<x-layouts.app :title="__('Contacts')">
+    <livewire:add-contact />
+    <livewire:contact-table/>
+</x-layouts.app>
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+    Route::get('/contacts', function () {
+        return view('contactss');
+    })->name('contactss');
 });
 
 require __DIR__.'/auth.php';
